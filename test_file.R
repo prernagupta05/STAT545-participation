bb2a414e144f0f693a92549ead8965078c8b1c37
Prerna Gupta <prernagupta510@gmail.com>
2019-10-08 10:51:43 -0700

    class meeting 11 things

---
+++ test_file.R
@@ -0,0 +1,23 @@
+library("tidyverse")
+library("gapminder")
+
+gapminder
+write_csv(gapminder, './gapminder.csv')
+View(gapminder)
+
+gapminder_sum <- gapminder %>%
+                group_by(continent) %>%
+                summarize(avg_lifeExp = mean(lifeExp))
+
+View(gapminder_sum)
+write_csv(gapminder_sum, here::here("./gapminder_sum.csv"))
+
+gapminder_sum %>%
+  ggplot(aes(x = continent,
+             y = avg_lifeExp)) +
+  geom_point() +
+  theme_bw()
+
+
+
+
